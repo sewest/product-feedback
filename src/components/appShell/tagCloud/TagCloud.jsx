123d0febb439a04c7e4437c4a5ab17f1d280add0
Sean <seanswest30@gmail.com>
2023-09-07 11:15:32 -0500

    Create tag cloud component and styles

---
+++ src/components/appShell/tagCloud/TagCloud.jsx
@@ -0,0 +1,22 @@
+import data from "../../../assets/data/data.json";
+import Chip from "../../chip/Chip";
+import styles from "./tagCloud.module.css";
+
+export default function TagCloud() {
+  const uniqueCategories = new Set();
+
+  return (
+    <div className={styles.tagCloud}>
+      <Chip key="All">All</Chip>
+      <Chip key="UI">UI</Chip>
+      <Chip key="UX">UX</Chip>
+      {data.productRequests.map((item) => {
+        if (!uniqueCategories.has(item.category)) {
+          uniqueCategories.add(item.category);
+          return <Chip key={item.id}>{item.category.charAt(0).toUpperCase() + item.category.slice(1)}</Chip>;
+        }
+        return null;
+      })}
+    </div>
+  );
+}
