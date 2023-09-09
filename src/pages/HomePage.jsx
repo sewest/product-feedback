9aab090befb119ea958a9cbca0e383564d59ad8e
Sean <seanswest30@gmail.com>
2023-09-09 17:44:54 -0500

    Add suggestions to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import Suggestions from "../components/suggestions/Suggestions";
+
 /**
  * Renders the HomePage component.
  *
  * @return {JSX.Element} The rendered JSX element.
  */
 export default function HomePage() {
-  return <h1>HomePage</h1>;
+  return <Suggestions />;
 }
