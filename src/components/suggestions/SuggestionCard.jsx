0a36bf22e152d3ffe27ad1d340ffa038d67871fb
Sean <seanswest30@gmail.com>
2023-09-13 13:49:08 -0500

    Adjust suggestions and suggestions suggestion card
    - Update basic button usage and import path
    - Add id, key, and replies props to suggestion card
    - Update the way the number of comments is calculated

--- src/components/suggestions/SuggestionCard.jsx
+++ src/components/suggestions/SuggestionCard.jsx
@@ -1,4 +1,5 @@
-import UpvoteButton from "../buttons/UpvoteButton";
+import { Link } from "react-router-dom";
+import UpvoteButton from "../buttons/upvoteButton/UpvoteButton";
 import Chip from "../../components/chip/Chip";
 import { ReactComponent as Comments } from "../../assets/icons/iconComments.svg";
 import styles from "./suggestionCard.module.css";
@@ -13,29 +14,31 @@ import styles from "./suggestionCard.module.css";
  * @param {number} sugComments - The number of comments on the suggestion.
  * @return {ReactElement} The rendered suggestion card component.
  */
-export default function SuggestionCard({ sugTitle, sugDescription, sugCategory, sugVotes, sugComments }) {
+export default function SuggestionCard({ sugTitle, sugDescription, sugCategory, sugVotes, sugComments, sugId, sugReplies }) {
   return (
-    <article className={styles.suggestionCard}>
-      <div className={styles.outerButtonContainer}>
-        <UpvoteButton count={sugVotes} />
-      </div>
-
-      <div className={styles.cardHeader}>
-        <h2>{sugTitle}</h2>
-        <p>{sugDescription}</p>
-        <Chip>{sugCategory}</Chip>
-      </div>
-      <div className={styles.buttonContainer}>
-        <UpvoteButton count={sugVotes} />
-        <div>
-          <Comments />
-          {sugComments}
+    <Link to={`/feedback/${sugId}`} className={styles.suggestionCardLink}>
+      <article className={styles.suggestionCard}>
+        <div className={styles.outerButtonContainer}>
+          <UpvoteButton count={sugVotes} />
         </div>
-      </div>
-      <div className={styles.outerCommentsContainer}>
-        <Comments />
-        {sugComments}
-      </div>
-    </article>
+
+        <div className={styles.cardHeader}>
+          <h2>{sugTitle}</h2>
+          <p>{sugDescription}</p>
+          <Chip>{sugCategory}</Chip>
+        </div>
+        <div className={styles.buttonContainer}>
+          <UpvoteButton count={sugVotes} />
+          <div>
+            <Comments />
+            {sugComments}
+          </div>
+        </div>
+        <div className={`${sugReplies + sugComments || styles.zeroReplies} ${styles.outerCommentsContainer}`}>
+          <Comments />
+          {sugComments + sugReplies || 0}
+        </div>
+      </article>
+    </Link>
   );
 }
--- src/components/suggestions/Suggestions.jsx
+++ src/components/suggestions/Suggestions.jsx
@@ -2,23 +2,39 @@ import Toolbar from "../toolBar/ToolBar";
 import SuggestionCard from "../suggestions/SuggestionCard";
 import data from "../../assets/data/data.json";
 import { ReactComponent as Empty } from "../../assets/icons/iconEmpty.svg";
-import BasicButton from "../buttons/BasicButton";
+import BasicButton from "../buttons/basicButton/BasicButton";
 import styles from "./suggestions.module.css";
 
+/**
+ * Renders the Suggestions component.
+ *
+ * @return {JSX.Element} The rendered Suggestions component.
+ */
 export default function Suggestions() {
   const reqs = data.productRequests;
-  // const reqs = [];
 
   return (
     <section className={styles.suggestions}>
       <Toolbar />
       {reqs.length > 0 && (
         <ul className={styles.suggestionsList}>
-          {reqs.map((req) => (
-            <li key={req.id}>
-              <SuggestionCard sugTitle={req.title} sugDescription={req.description} sugCategory={req.category} sugVotes={req.upvotes} sugComments={req.comments ? req.comments.length : 0} />
-            </li>
-          ))}
+          {reqs.map(
+            (req) =>
+              req.status === "suggestion" && (
+                <li key={req.id}>
+                  <SuggestionCard
+                    sugTitle={req.title}
+                    sugDescription={req.description}
+                    sugCategory={req.category}
+                    sugVotes={req.upvotes}
+                    sugComments={req.comments ? req.comments.length : 0}
+                    sugId={req.id}
+                    key={req.id}
+                    sugReplies={req.comments && getTotalRepliesCount(req.comments)}
+                  />
+                </li>
+              )
+          )}
         </ul>
       )}
       {reqs.length === 0 && (
@@ -26,9 +42,19 @@ export default function Suggestions() {
           <Empty />
           <h2>There is no feedback yet.</h2>
           <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
-          <BasicButton type={"button1"}>+ Add Feedback</BasicButton>
+          <BasicButton buttonType={"button1"}>+ Add Feedback</BasicButton>
         </section>
       )}
     </section>
   );
 }
+
+/**
+ * Calculates the total number of replies in the given comments array.
+ *
+ * @param {Array} comments - An array of comments.
+ * @return {number} The total number of replies.
+ */
+const getTotalRepliesCount = (comments) => {
+  return comments.reduce((count, comment) => count + (comment.replies ? comment.replies.length : 0), 0);
+};
