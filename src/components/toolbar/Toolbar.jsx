5cb090809f304f830dd456733953530fbbd8bb62
Sean <seanswest30@gmail.com>
2023-09-13 13:36:25 -0500

    Change button 'type' prop to 'buttonType and adjust import location

--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -1,7 +1,7 @@
 import Dropdown from "../dropdown/Dropdown";
 import styles from "./toolbar.module.css";
 import { ReactComponent as Bulb } from "../../assets/icons/iconBulb.svg";
-import BasicButton from "../buttons/BasicButton";
+import BasicButton from "../buttons/basicButton/BasicButton";
 
 /**
  * Renders a toolbar component.
@@ -16,7 +16,7 @@ export default function Toolbar() {
         <h2>6 Suggestions</h2>
       </div>
       <Dropdown />
-      <BasicButton type={"button1"} style={{ marginLeft: "auto" }}>
+      <BasicButton buttonType={"button1"} style={{ marginLeft: "auto" }}>
         + Add Feedback
       </BasicButton>
     </menu>
