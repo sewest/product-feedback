177b108fb82018125a7c9bc0f607222ed28544a5
Sean <seanswest30@gmail.com>
2023-09-19 11:26:48 -0500

    Add content to roadmap page

--- src/pages/RoadMapPage.jsx
+++ src/pages/RoadMapPage.jsx
@@ -1,8 +1,17 @@
+import Toolbar from "../components/toolbar/Toolbar";
+import Tabs from "../components/tabs/Tabs";
+import RoadmapGrid from "../components/roadmapGrid/RoadmapGrid";
 /**
  * Renders the RoadmapPage component, which displays the main roadmap page.
  *
  * @return {JSX.Element} The JSX element representing the RoadmapPage component.
  */
 export default function RoadmapPage() {
-  return <h1>RoadmapPage</h1>;
+  return (
+    <>
+      <Toolbar />
+      <Tabs />
+      <RoadmapGrid />
+    </>
+  );
 }
