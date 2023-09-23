023fce09d32ea041c3ce659c82cea8dd34619ef6
sewest <seanswest30@gmail.com>
2023-09-23 10:59:06 -0500

    Refactor toolbar
    - Add markup for roadmap page
    - Add conditional rendering based on path
    - Add function to calculate number of suggestions
    - Add styles for roadmap page

--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -1,8 +1,12 @@
+import { useLocation } from "react-router-dom";
+import { ReactComponent as Bulb } from "../../assets/icons/iconBulb.svg";
+import { ReactComponent as Chevron } from "../../assets/icons/iconArrowLeft.svg";
+import data from "../../assets/data/data.json";
+import FeedbackModal from "../feedbackModal/FeedbackModal";
+import IconButton from "../buttons/iconButton/IconButton";
+import Title from "../title/Title";
 import Dropdown from "../dropdown/Dropdown";
 import styles from "./toolbar.module.css";
-import { ReactComponent as Bulb } from "../../assets/icons/iconBulb.svg";
-import FeedbackModal from "../feedbackModal/FeedbackModal";
-import Title from "../title/Title";
 
 /**
  * Renders a toolbar component.
@@ -10,15 +14,27 @@ import Title from "../title/Title";
  * @return {ReactElement} The rendered toolbar.
  */
 export default function Toolbar() {
+  const path = useLocation().pathname;
+
   return (
-    <menu className={styles.toolbar}>
-      <div className={styles.headerContainer}>
+    <menu className={`${path === "/" ? styles.mainToolbar : styles.roadmapToolbar}`}>
+      {/* Show Suggestions on suggestions path  */}
+      <div className={`${path !== "/" ? styles.hidden : ""} ${styles.headerContainer}`}>
         <Bulb />
         <Title order={2} size="lg" color="light">
-          FIX ME Suggestions
+          {getSuggestionCount(data.productRequests)} Suggestions
         </Title>
       </div>
-      <Dropdown />
+      {/* Show Roadmap on roadmap path */}
+      <div className={`${path !== "/roadmap" ? styles.hidden : ""} ${styles.roadmapHeader}`}>
+        <IconButton icon={<Chevron />} to={"/"} isLink classes={styles.backButton}>
+          Go Back
+        </IconButton>
+        <Title order={2} size="lg" color="light">
+          Roadmap
+        </Title>
+      </div>
+      <Dropdown classes={path === "/roadmap" ? styles.hidden : ""} />
 
       <FeedbackModal buttonType={"button1"} style={{ marginLeft: "auto", justifySelf: "end" }}>
         + Add Feedback
@@ -26,3 +42,13 @@ export default function Toolbar() {
     </menu>
   );
 }
+
+/**
+ * Returns the count of items in the given data array that have a status of "suggestion".
+ *
+ * @param {Array} data - The array of items to be filtered.
+ * @return {number} The count of items with a status of "suggestion".
+ */
+const getSuggestionCount = (data) => {
+  return data.filter((item) => item.status === "suggestion").length;
+};
