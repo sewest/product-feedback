005bf235dc9413a17c2cb388c4341456e677ceb7
sewest <seanswest30@gmail.com>
2023-09-23 15:48:16 -0500

    Refactor comments main and feedback page for use with context

--- src/pages/FeedbackPage.jsx
+++ src/pages/FeedbackPage.jsx
@@ -1,5 +1,3 @@
-import { useLoaderData, useParams } from "react-router";
-import { useAppState } from "../context/AppContext";
 import CommentsMain from "../components/comments/CommentsMain";
 /**
  * Renders the RoadmapPage component, which displays the main roadmap page.
@@ -7,14 +5,5 @@ import CommentsMain from "../components/comments/CommentsMain";
  * @return {JSX.Element} The JSX element representing the RoadmapPage component.
  */
 export default function FeedbackPage() {
-  const { feedbackId } = useParams();
-  const state = useAppState();
-  const req = state.productRequests.find((req) => req.id === +feedbackId);
-  const comments = req.comments || [];
-
-  const data = {
-    req,
-    comments,
-  };
-  return <CommentsMain data={data} />;
+  return <CommentsMain />;
 }
